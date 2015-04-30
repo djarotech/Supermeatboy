6499eb5f4ad65c17788d1b8957444297312b0e7f
Kevin Mao <mk217187@gmail.com>
2015-04-30 02:28:17 -0500

    Finally got platforms to work somewhat
    framerates are janky and detection is difficult due to multiple threads
    running at once. i had made the hitboxes on the block huge. plus its
    flashing. fix?
    
    *i commented on the things that i changed
    **is this how you do it daniel?

--- SupermeatBoy/src/Platform.java
+++ SupermeatBoy/src/Platform.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.Image;
@@ -14,51 +15,72 @@ import javax.swing.JFrame;
 
 public class Platform implements Runnable {
 	private MeatBoy meatBoy;
-	private int XCord;
-	private int YCord;
+	private int xCoord;
+	private int yCoord;
 	private int width;
 	private int height;
 	private Color color;
-	private String MeatBoyStatus;
 	
-	public Platform(Component c, int w, int h, int x, int y){
-		frame_height=c.getHeight();
-		frame_width=c.getWidth();
-		xCord = x;
-		yCord = y;
+	public Platform(MeatBoy m, Component c, int w, int h, int x, int y){
+		color = Color.GREEN;
+		meatBoy = m;
+		meatBoy.setMeatBoyStatus("none");
+		xCoord = x;
+		yCoord = y;
 		width = w;
 		height = h;
-		MeatBoyStatus = "notTouching";
-		platformHitbox = new Rectangle(xCoord,yCoord,length,width);
+		Rectangle platformHitbox = new Rectangle(xCoord,yCoord,width,height);
 	}
 	public void draw(Graphics g) throws IOException{
 		g.setColor(color);
-		g.drawRect(XCord,YCord,width,height)
+		g.fillRect(xCoord,yCoord,width,height);
 	}
 		
 	public void run() {
 		//Checks if meatboy is within its parameters and stops velocity if so
 		color = Color.RED;
-		if ((MeatBoy.getXPos()==xCord || MeatBoy.getXPos()==xCord+length) && MeatBoy.getYPos()>yCord && MeatBoy.getYPos()<yCord+width)
+//		if ((meatBoy.getXPos()>=xCoord || meatBoy.getXPos()<=xCoord+width) && meatBoy.getYPos()>yCoord && meatBoy.getYPos()<yCoord+width)
+//		{
+//			meatBoy.setXVel(0);
+//			if (meatBoy.getXPos()==xCoord)
+//				MeatBoyStatus = "LeftWall";
+//			else
+//				MeatBoyStatus = "RightWall";
+//		}
+//		else if (meatBoy.getXPos()>=xCoord && meatBoy.getXPos()<=xCoord+width && (meatBoy.getYPos()>=yCoord || meatBoy.getYPos()<=yCoord+height))
+//		{
+//			if (meatBoy.getYPos()==yCoord)
+//				MeatBoyStatus = "Grounded";
+//			meatBoy.setYVel(0);
+//		}
+		//left wall case
+		if ((meatBoy.getXPos()+20>=xCoord-21 && meatBoy.getXPos()+20<=xCoord+21) && meatBoy.getYPos()>yCoord && meatBoy.getYPos()<yCoord+height)
 		{
-			MeatBoy.setXVel(0);
-			if (MeatBoy.getXPos()==xCord)
-				MeatBoyStatus = "LeftWall";
-			else
-				MeatBoyStatus = "RightWall";
+			meatBoy.setMeatBoyStatus("LeftWall");
+			
 		}
-		else if (MeatBoy.getXPos()>=xCord && MeatBoy.getXPos()<=xCord+length && (MeatBoy.getYPos()==yCord || MeatBoy.getYPos()==yCord+width))
+		//right wall case;
+		else if ((meatBoy.getXPos()<=xCoord+width+21 && meatBoy.getXPos()>=xCoord+width-21) && meatBoy.getYPos()>yCoord && meatBoy.getYPos()<yCoord+height)
 		{
-			if (MeatBoy.getYPos()==YCord)
-				MeatBoyStatus = "Grounded";
-			MeatBoy.setYVel(0);
+			meatBoy.setMeatBoyStatus("RightWall");
 		}
+		//top case
+		else if (meatBoy.getXPos()>=xCoord && meatBoy.getXPos()<=xCoord+width && (meatBoy.getYPos()+20>=yCoord-21 && meatBoy.getYPos()+20<=yCoord+21))
+		{
+			meatBoy.setMeatBoyStatus("Top");
+		}
+		//bottom case
+		else if (meatBoy.getXPos()>=xCoord && meatBoy.getXPos()<=xCoord+width && (meatBoy.getYPos()<=yCoord+height+21 && meatBoy.getYPos()>=yCoord+height-21))
+		{
+			meatBoy.setMeatBoyStatus("Bottom");
+		}
+		
 		else
 		{
-			MeatBoyStatus = "notTouching";
+			meatBoy.setMeatBoyStatus("none");
 			color = Color.green;
 		}
 		}
-	}
+
 
 }
